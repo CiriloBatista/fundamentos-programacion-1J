96fb8adee118b5c31d34b9b1b4734b9db1c76ed3
Cirilo Batista Ochoa <cirilo.8475174@gmail.com>
2018-10-06 16:30:33 -0500

    Nuevo ejercicio con condiciones

---
+++ unidad-3/ejemplo-3/ejercicio3.js
@@ -0,0 +1,43 @@
+//Variables.....
+var inputNumero = document.querySelector('#numero');
+var numero = Number(inputNumero.value);
+
+var pResultado = document.querySelector('#resultado');
+
+var opciones = document.querySelector('#opciones');
+opciones.addEventListener('change', evalOpcion);
+
+//Funciones......
+function evalOpcion(){
+    var numero = Number(inputNumero.value);
+    var opcion = opciones.value;
+
+    if (opcion === 'positivo'){
+        if(numero > 0) {
+            pResultado.textContent = 'El número '+ numero +' es positivo';
+        }
+        else if(numero < 0) {
+            pResultado.textContent = 'El número '+ numero +' es negativo';
+        }
+        else{
+            pResultado.textContent = 'El número es 0';
+        }
+    }
+    else if (opcion === 'mitad'){
+        pResultado.textContent = 'La mitad del número '+ numero +' es '+(numero/2);
+    }
+    else if(opcion === 'cuadrado'){
+        pResultado.textContent = 'El cuadrado del número '+ numero +' es '+ (numero*numero);
+    }
+    else if(opcion === 'par'){
+        if(numero%2 === 0){
+            pResultado.textContent = 'El número es par';
+        }
+        else{
+            pResultado.textContent = 'El número es impar';
+        }
+    }
+    else{
+        pResultado.textContent = 'No ha seleccionado nada';
+    }
+}
